e52c20b275403a83546b5c480a446d6f5650b295
Elliot Tikhomirov <tikhomirovi@icloud.com>
2020-08-30 18:55:35 +0300

    Fix locale bug

--- example/research_package_demo_app/lib/main.dart
+++ example/research_package_demo_app/lib/main.dart
@@ -27,20 +27,20 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
       ],
       // Returns a locale which will be used by the app
-      localeResolutionCallback: (locale, supportedLocales) {
-        // Check if the current device locale is supported
-        for (var supportedLocale in supportedLocales) {
-          if (supportedLocale.languageCode == locale.languageCode
-              /*  && supportedLocale.countryCode == locale.countryCode */
-              // TODO: Test on physical iPhone if Locale should use countryCode instead
-              ) {
-            return supportedLocale;
-          }
-        }
-        // If the locale of the device is not supported, use the first one
-        // from the list (English, in this case).
-        return supportedLocales.first;
-      },
+      // localeResolutionCallback: (locale, supportedLocales) {
+      //   // Check if the current device locale is supported
+      //   for (var supportedLocale in supportedLocales) {
+      //     if (supportedLocale.languageCode == locale.languageCode
+      //         /*  && supportedLocale.countryCode == locale.countryCode */
+      //         // TODO: Test on physical iPhone if Locale should use countryCode instead
+      //         ) {
+      //       return supportedLocale;
+      //     }
+      //   }
+      //   // If the locale of the device is not supported, use the first one
+      //   // from the list (English, in this case).
+      //   return supportedLocales.first;
+      // },
       theme: ThemeData(
         appBarTheme: AppBarTheme(color: Colors.transparent, elevation: 0),
         primarySwatch: velmioColor,
